4d0770ed5b3d694038294bfde336a5bcd5d9fd0e
Ethan Robinson <toddthemaddog@gmail.com>
2016-10-23 20:12:08 -0700

    Created .cpp file with definitions. All definitions are empty

---
+++ Project3/hash.cpp
@@ -0,0 +1,35 @@
+#include "hash.h"
+
+Hash::Hash()
+{
+}
+
+void Hash::remove(string)
+{
+}
+
+void Hash::print()
+{
+}
+
+void Hash::processFile(string)
+{
+}
+
+bool Hash::search(string)
+{
+	return false;
+}
+
+void Hash::output(string)
+{
+}
+
+void Hash::printStats()
+{
+}
+
+int Hash::hf(string)
+{
+	return 0;
+}
